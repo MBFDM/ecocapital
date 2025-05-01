c7ed82ffa1508a98bf7e3a8812cdda8fde516b20
MBFDM <119826728+MBFDM@users.noreply.github.com>
2025-05-01 22:29:47 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
@@ -15,6 +14,14 @@ from PIL import Image
 
 # Vérification de l'authentification
 check_authentication()
+
+# Ajoutez ceci au début de votre script
+st.session_state.setdefault('force_refresh', True)
+
+if st.session_state.force_refresh:
+    time.sleep(0.1)  # Pause minimale
+    st.session_state.force_refresh = False
+    st.rerun()  # Force le rechargement propre
 
 # Configuration de la page
 st.set_page_config(
